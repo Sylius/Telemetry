698a98422b8f8af65f5443b14417d86b108ebd4c
TheMilek <kam.grygierzec@gmail.com>
2026-05-08 12:14:59 +0200

    Forward plugin-side activation through TelemetryCompilerPass

--- src/TelemetryCompilerPass.php
+++ src/TelemetryCompilerPass.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace Sylius\Telemetry;
 
-use Sylius\Bundle\CoreBundle\SyliusCoreBundle;
 use Sylius\Component\Core\Telemetry\TelemetrySendManagerInterface;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Loader\DelegatingLoader;
-use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class TelemetryCompilerPass implements CompilerPassInterface
@@ -32,31 +27,13 @@ class TelemetryCompilerPass implements CompilerPassInterface
             return;
         }
 
-        $this->loadTelemetryServices($container);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader->load('services.xml');
     }
 
     protected function isTelemetrySupported(): bool
     {
-        return interface_exists(TelemetrySendManagerInterface::class);
-    }
-
-    private function loadTelemetryServices(ContainerBuilder $container): void
-    {
-        $bundleDir = dirname((new \ReflectionClass(SyliusCoreBundle::class))->getFileName());
-        $configDir = $bundleDir . '/Resources/config/services/telemetry';
-
-        $locator = new FileLocator($configDir);
-
-        $loaders = [
-            new PhpFileLoader($container, $locator),
-            new DirectoryLoader($container, $locator),
-        ];
-
-        if (class_exists(XmlFileLoader::class)) {
-            $loaders[] = new XmlFileLoader($container, $locator);
-        }
-
-        $loader = new DelegatingLoader(new LoaderResolver($loaders));
-        $loader->load($configDir, 'directory');
+        return interface_exists(TelemetrySendManagerInterface::class)
+            || interface_exists(\Sylius\Telemetry\TelemetrySendManagerInterface::class);
     }
 }
