f7c45064533a4edce2b707e0f3ab7fbc2497c218
TheMilek <kam.grygierzec@gmail.com>
2026-05-08 12:14:59 +0200

    Add host-side activation via LoadTelemetryServicesPass

---
+++ src/LoadTelemetryServicesPass.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Telemetry;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+final class LoadTelemetryServicesPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasParameter('sylius_core.telemetry.url')) {
+            return;
+        }
+
+        if (!$container->getParameter('sylius_core.telemetry.enabled')) {
+            return;
+        }
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader->load('services.xml');
+    }
+}
